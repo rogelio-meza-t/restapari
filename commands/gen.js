a2ca5bff73bde4a316b5666d4092ce583efafb90
Rogelio Meza <rogelio.meza.t@gmail.com>
2022-10-15 00:29:24 -0300

    check if definitions file exists

--- commands/gen.js
+++ commands/gen.js
@@ -1,5 +1,23 @@
-const gen = () => {
+import { existsSync } from 'node:fs';
+
+import chalk from "chalk";
+
+export default () => {
+    // TODO: 20221015 get filename from arguments
+    //       if not present, set default name
+
+    const path = "./definitions.yml"
+
+    try{
+        if (existsSync(path)) {
+            //file exists
+        } else {
+            console.log(
+                chalk.red.bold("Definitions file doesn't exists")
+            )
+        }
+    } catch(err) {
+        console.error(err)
+    }
 
 }
-
-module.exports = gen
